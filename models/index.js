7980fdd983a50dc075d947a3a0a08a9349027d61
Finn Terdal <fterdal@gmail.com>
2017-08-04 17:03:13 -0500

    Adds models for Page and User

--- models/index.js
+++ models/index.js
@@ -1,4 +1,37 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
-module.exports = db;
+/*
+[a-z\-_\(\)]*
+*/
+const Page = db.define("page", {
+  title: {
+    type: Sequelize.STRING,
+  },
+  urlTitle: {
+    type: Sequelize.STRING,
+    validate: {
+      is: /[a-z\-_\(\)]*/i
+    }
+  },
+  content: {
+    type: Sequelize.TEXT
+  },
+  status: {
+    type: Sequelize.ENUM("open", "closed")
+  }
+});
+
+const User = db.define("user", {
+  name: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+module.exports = { Page, User };
